3c98810c6e10850823d20f0a52c725fad1d6a7c0
FlawKill <ayushdayal7300@gmail.com>
2024-08-28 01:17:50 +0530

    UI changes

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -8,6 +8,7 @@ import CryptoJS from 'crypto-js';
 const Signin = ({ switchToSignup, switchToResetPassword }) => {
   const [signinData, setSigninData] = useState({ email: '', password: '' });
   const [errorMessage, setErrorMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // State for toggling password visibility
   const navigate = useNavigate();
   const setToken = useSetRecoilState(token);
 
@@ -44,28 +45,71 @@ const Signin = ({ switchToSignup, switchToResetPassword }) => {
 
   return (
     <div className='flex flex-col items-center bg-white p-10 rounded-lg shadow-lg w-full max-w-md'>
-      <h2 className="mb-6 text-black text-3xl font-semibold">Sign In</h2>
+      <h2 className="mb-6 text-black text-3xl font-semibold">Login</h2>
       <form onSubmit={handleSigninSubmit} className="w-full flex flex-col">
-        <input
+      <input
           type="email"
           name="email"
           placeholder="Email"
           value={signinData.email}
           onChange={handleSigninChange}
           required
-          className="p-4 my-2 border border-gray-600 rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
+          className="p-4 my-2 border border-black rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
           aria-label="Email"
         />
+        <div>
+          <svg 
+            className="relative -mt-9 left-80 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-800 cursor-pointer hover:scale-105" 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 48 48"
+          >
+            <path stroke="currentColor" strokeWidth="2" d="M45,7H3a3,3,0,0,0-3,3V38a3,3,0,0,0,3,3H45a3,3,0,0,0,3-3V10A3,3,0,0,0,45,7Zm-.64,2L24,24.74,3.64,9ZM2,37.59V10.26L17.41,22.17ZM3.41,39,19,23.41l4.38,3.39a1,1,0,0,0,1.22,0L29,23.41,44.59,39ZM46,37.59,30.59,22.17,46,10.26Z"/>
+          </svg>
+        </div>
+
         <input
-          type="password"
+          type={showPassword ? "text" : "password"}  // Toggle input type based on showPassword state
           name="password"
           placeholder="Password"
           value={signinData.password}
           onChange={handleSigninChange}
           required
-          className="p-4 my-2 border border-gray-600 rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
+          className="p-4 my-2 border border-black rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
           aria-label="Password"
         />
+        <div>
+          {showPassword ? (
+            <svg
+              onClick={() => setShowPassword(false)}
+              className="relative -mt-9 left-80 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-800 cursor-pointer hover:scale-105"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path stroke="currentColor" strokeWidth="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+              <path stroke="currentColor" strokeWidth="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              <line x1="1" y1="1" x2="23" y2="23" stroke="currentColor" strokeWidth="2" />
+            </svg>
+          ) : (
+            <svg
+              onClick={() => setShowPassword(true)}
+              className="relative -mt-9 left-80 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-800 cursor-pointer hover:scale-105"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path stroke="currentColor" strokeWidth="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+              <path stroke="currentColor" strokeWidth="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+          )}
+        </div>
+
         <button
           type="button"
           className="bg-transparent text-blue-500 p-2 ml-auto mt-2 border-0 border-transparent hover:border-blue-500 hover:text-blue-700 transition-colors duration-300"
@@ -89,7 +133,7 @@ const Signin = ({ switchToSignup, switchToResetPassword }) => {
           className="bg-transparent text-blue-500 p-2 border-0 border-transparent hover:border-blue-500 hover:text-blue-700 transition-colors duration-300"
           onClick={switchToSignup} 
         >
-          Sign up
+          Register
         </button>
       </p>
     </div>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -5,6 +5,9 @@ import CryptoJS from 'crypto-js';
 const Signup = ({ setShowOTP, setSignupData, showOTP, switchToSignin }) => {
   const [localSignupData, setLocalSignupData] = useState({ username: '', email: '', password: '' });
   const [otp, setOTP] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+  const [successMessage, setSuccessMessage] = useState(''); // State to manage success message
+  const [isRegistered, setIsRegistered] = useState(false); // State to manage button visibility
 
   const handleSignupChange = (e) => {
     setLocalSignupData({ ...localSignupData, [e.target.name]: e.target.value });
@@ -29,6 +32,7 @@ const Signup = ({ setShowOTP, setSignupData, showOTP, switchToSignin }) => {
       if (response.status === 200) {
         setShowOTP(true);
         setSignupData(localSignupData);
+        setIsRegistered(true); // Hide the Register button
         console.log(response.data.msg);
       }
     } catch (error) {
@@ -45,7 +49,10 @@ const Signup = ({ setShowOTP, setSignupData, showOTP, switchToSignin }) => {
         otp: otp
       });
       if (response.status === 200) {
-        console.log(response.data.message);
+        setSuccessMessage('Account created successfully! Redirecting to sign in...');
+        setTimeout(() => {
+          switchToSignin();
+        }, 2000); // Redirect after 2 seconds
       } else if (response.status === 401) {
         console.log(response.data.message);
       }
@@ -56,44 +63,103 @@ const Signup = ({ setShowOTP, setSignupData, showOTP, switchToSignin }) => {
 
   return (
     <div className='flex flex-col items-center bg-white p-10 rounded-lg shadow-lg w-full max-w-md'>
-      <h2 className='mb-5 text-black text-center text-3xl font-semibold'>Sign up</h2>
-      <form onSubmit={handleSignupSubmit} className='w-full flex flex-col'>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={localSignupData.username}
-          onChange={handleSignupChange}
-          required
-          className="p-4 my-2 border border-gray-600 rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={localSignupData.email}
-          onChange={handleSignupChange}
-          required
-          className="p-4 my-2 border border-gray-600 rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={localSignupData.password}
-          onChange={handleSignupChange}
-          required
-          className="p-4 my-2 border border-gray-600 rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
-        />
-        <button 
-          type="submit" 
-          className="p-4 mt-4 bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
-        >
-          Register
-        </button>
-      </form>
-      {showOTP && (
-        <form onSubmit={handleOTPSubmit} className='w-full flex flex-col mt-4'>
+      <h2 className='mb-5 text-black text-center text-3xl font-semibold'>Register</h2>
+      {!isRegistered ? (
+        <form onSubmit={handleSignupSubmit} className='w-full flex flex-col'>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={localSignupData.username}
+            onChange={handleSignupChange}
+            required
+            className="p-4 my-2 border border-black rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
+          />
+          <div>
+            <svg 
+              className="relative -mt-9 left-80 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-800 cursor-pointer hover:scale-105" 
+              aria-hidden="true" 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="24" 
+              height="24" 
+              fill="none" 
+              viewBox="0 0 24 24"
+            >
+              <path stroke="currentColor" strokeWidth="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+            </svg>
+          </div>
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={localSignupData.email}
+            onChange={handleSignupChange}
+            required
+            className="p-4 my-2 border border-black rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
+          />
+          <div>
+            <svg 
+              className="relative -mt-9 left-80 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-800 cursor-pointer hover:scale-105" 
+              xmlns="http://www.w3.org/2000/svg" 
+              viewBox="0 0 48 48"
+            >
+              <path stroke="currentColor" strokeWidth="2" d="M45,7H3a3,3,0,0,0-3,3V38a3,3,0,0,0,3,3H45a3,3,0,0,0,3-3V10A3,3,0,0,0,45,7Zm-.64,2L24,24.74,3.64,9ZM2,37.59V10.26L17.41,22.17ZM3.41,39,19,23.41l4.38,3.39a1,1,0,0,0,1.22,0L29,23.41,44.59,39ZM46,37.59,30.59,22.17,46,10.26Z"/>
+            </svg>
+          </div>
+
+          <input
+            type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
+            name="password"
+            placeholder="Password"
+            value={localSignupData.password}
+            onChange={handleSignupChange}
+            required
+            className="p-4 my-2 border border-black rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
+          />
+          <div>
+            {showPassword ? (
+              <svg
+                onClick={() => setShowPassword(false)}
+                className="relative -mt-9 left-80 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-800 cursor-pointer hover:scale-105"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path stroke="currentColor" strokeWidth="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                <path stroke="currentColor" strokeWidth="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <line x1="1" y1="1" x2="23" y2="23" stroke="currentColor" strokeWidth="2" />
+              </svg>
+            ) : (
+              <svg
+                onClick={() => setShowPassword(true)}
+                className="relative -mt-9 left-80 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-800 cursor-pointer hover:scale-105"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path stroke="currentColor" strokeWidth="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                <path stroke="currentColor" strokeWidth="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+            )}
+          </div>
+      
+          <button 
+            type="submit" 
+            className="p-4 mt-4 bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
+          >
+            Register
+          </button>
+        </form>
+      ) : (
+        <form onSubmit={handleOTPSubmit} className='w-full flex flex-col'>
+          <h3 className='mt-3 mb-2 -ml-60 font-semibold text-lg text-black'>OTP Verification</h3>
           <input
             type="text"
             name="otp"
@@ -101,25 +167,22 @@ const Signup = ({ setShowOTP, setSignupData, showOTP, switchToSignin }) => {
             value={otp}
             onChange={(e) => setOTP(e.target.value)}
             required
-            className="p-4 my-2 border border-gray-600 rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
+            className="p-4 my-2 border border-black rounded-lg bg-white text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
           />
           <button 
             type="submit" 
-            className='p-3 mt-3 bg-green-600 text-white rounded cursor-pointer hover:bg-green-500 focus:outline-none focus:transform focus:scale-105 transition'
+            className="p-4 mt-2 bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform transform hover:scale-105"
           >
             Verify OTP
           </button>
         </form>
       )}
-      <h3 className='mt-4 text-black'>
-        Already have an Account? 
-        <button 
-          className="bg-transparent text-blue-500 p-2 border-0 border-transparent hover:border-blue-500 hover:text-blue-700 transition-colors duration-300"
-          onClick={switchToSignin}
-        >
-          Log in
-        </button>
-      </h3>
+
+      {successMessage && (
+        <div className="mt-4 p-4 text-green-600 bg-green-100 rounded-lg">
+          {successMessage}
+        </div>
+      )}
     </div>
   );
 };
