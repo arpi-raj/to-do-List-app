f2546afaa83eedc30cdb11c40c33a76aa3a7685c
Arpit Raj <er.arpitraj06@gmail.com>
2024-09-04 16:41:40 +0530

    profile completed and auto login

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,14 +1,38 @@
+import { useRecoilValue } from "recoil";
+import { userState } from "../../store/atoms/states";
+
 export default function Profile() {
+  const { userName, userEmail } = useRecoilValue(userState);
+  console.log(userEmail + userName);
   return (
-    <div className="bg-red-700" >
-      <div className="bg-red-700 flex items-center">
-        <button className="rounded-full my-1 mx-1 h-16 w-16 flex-shrink-0"></button>
-        <h1 className="flex ml-2">userName</h1>
-      </div>
-      <div>
-      <p className="text-2xl">Change Password</p>
-      <p className="text-2xl">Change User Name</p>
-      </div>
+    <div className="bg-red-700 my-2 h-30">
+      <form>
+        <div className="bg-red-700 flex items-center justify-center">
+          <button className="rounded-full text-3xl px-1  my-1 mx-1 h-20 w-20 flex-shrink-0">
+            {userName[0].toUpperCase()}
+          </button>
+          <div className="ml-2">
+            <h1 className="">{userName}</h1>
+            <h3 className="text-xl text-gray-200 text-left ml-1">
+              {userEmail}
+            </h3>
+          </div>
+          <button className="ml-auto bg-transparent border-hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.828 2.25c-.916 0-1.699.663-1.85 1.567l-.091.549a.798.798 0 0 1-.517.608 7.45 7.45 0 0 0-.478.198.798.798 0 0 1-.796-.064l-.453-.324a1.875 1.875 0 0 0-2.416.2l-.243.243a1.875 1.875 0 0 0-.2 2.416l.324.453a.798.798 0 0 1 .064.796 7.448 7.448 0 0 0-.198.478.798.798 0 0 1-.608.517l-.55.092a1.875 1.875 0 0 0-1.566 1.849v.344c0 .916.663 1.699 1.567 1.85l.549.091c.281.047.508.25.608.517.06.162.127.321.198.478a.798.798 0 0 1-.064.796l-.324.453a1.875 1.875 0 0 0 .2 2.416l.243.243c.648.648 1.67.733 2.416.2l.453-.324a.798.798 0 0 1 .796-.064c.157.071.316.137.478.198.267.1.47.327.517.608l.092.55c.15.903.932 1.566 1.849 1.566h.344c.916 0 1.699-.663 1.85-1.567l.091-.549a.798.798 0 0 1 .517-.608 7.52 7.52 0 0 0 .478-.198.798.798 0 0 1 .796.064l.453.324a1.875 1.875 0 0 0 2.416-.2l.243-.243c.648-.648.733-1.67.2-2.416l-.324-.453a.798.798 0 0 1-.064-.796c.071-.157.137-.316.198-.478.1-.267.327-.47.608-.517l.55-.091a1.875 1.875 0 0 0 1.566-1.85v-.344c0-.916-.663-1.699-1.567-1.85l-.549-.091a.798.798 0 0 1-.608-.517 7.507 7.507 0 0 0-.198-.478.798.798 0 0 1 .064-.796l.324-.453a1.875 1.875 0 0 0-.2-2.416l-.243-.243a1.875 1.875 0 0 0-2.416-.2l-.453.324a.798.798 0 0 1-.796.064 7.462 7.462 0 0 0-.478-.198.798.798 0 0 1-.517-.608l-.091-.55a1.875 1.875 0 0 0-1.85-1.566h-.344ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- frontend/store/atoms/states.jsx
+++ frontend/store/atoms/states.jsx
@@ -1,11 +1,21 @@
 import { atom } from 'recoil';
 
-export const token = atom({
-  key: 'token', 
-  default: localStorage.getItem('token') || '', // Initialize from localStorage
-});
+
+export const token = atom ({
+  key:"token",
+  default: ""
+})
+
 
 export const todoState = atom({
   key: 'todoState',
   default: [], // Default to an empty array
 });
+
+export const userState = atom ({
+  key:"userState",
+  default:{
+    userName:"",
+    userEmail:""
+  }
+})
