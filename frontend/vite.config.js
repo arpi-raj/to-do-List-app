e22d451b7dd744cae8f769eb5f1c4ee52d15169f
Arpit Raj <er.arpitraj06@gmail.com>
2025-02-17 15:49:32 +0530

    mime changes3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: '/src/main.js',
+      input: 'index.html',
     },
   },
 })
