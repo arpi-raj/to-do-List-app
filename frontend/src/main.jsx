6b78ccd880e4b33ab47cc8a2a64d7b0c68261e1e
Arpit Raj <er.arpitraj06@gmail.com>
2025-02-17 16:05:33 +0530

    mime changes5

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 // add the beginning of your app entry
 import "vite/modulepreload-polyfill";
 import App from "./App.jsx";
+import React from "react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
