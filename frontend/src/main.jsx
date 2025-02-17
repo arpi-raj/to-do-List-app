c8f3072f312e75f7f4e1dc40cc455e3b51ce80b0
Arpit Raj <er.arpitraj06@gmail.com>
2025-02-17 15:42:05 +0530

    mime changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+// add the beginning of your app entry
+import "vite/modulepreload-polyfill";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    mimeTypes: {
-      "application/javascript": ["js", "jsx"],
+    cors: {
+      // the origin you will be accessing via browser
+      origin: 'https://to-do-list-app-yjan.onrender.com',
     },
   },
-});
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/src/main.js',
+    },
+  },
+})
